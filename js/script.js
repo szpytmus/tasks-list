fe3d1ba5478581baf19f812c3eb77c56acca143a
Marcin Szpytma <ma.marcinsz@gmail.com>
2020-06-29 12:55:18 +0200

    adding new tasks

--- js/script.js
+++ js/script.js
@@ -1,42 +1,67 @@
 {
 
-const tasks = [
-    {
-        content: "nie wiem",
-        done: false,
-    
-    },
-    {
-        content: "nie wiem 2",
-        done: true,
-    },
-];
+    const tasks = [
+        {
+            content: "nie wiem",
+            done: false,
 
-    const render = () =>{
+        },
+        {
+            content: "nie wiem 2",
+            done: true,
+        },
+    ];
+
+
+    const addNewTask = (newTaskContent) => {
+
+        tasks.push({
+            content: newTaskContent,
+        });
+
+        render();
+    };
+
+
+    const render = () => {
         let htmlString = "";
 
-        for(const task of tasks){
+        for (const task of tasks) {
             htmlString += `
-            <li>
+            <li ${task.done ? " style=\"text-decoration:line-through\"" : ""}>
                 ${task.content}
             </li>
             `;
         }
 
-
-
-
-        
-    document.querySelector(".js-tasksList").innerHTML = htmlString;
+        document.querySelector(".js-tasksList").innerHTML = htmlString;
     };
+
+
+
+    const onFormSubmit = (event) => {
+        event.preventDefault();
+
+        const newTaskContent = document.querySelector(".js-newTask").value.trim();
+       
+        if(newTaskContent === ""){
+            return;
+        }
+
+
+        addNewTask(newTaskContent);
+    };
+
 
     const init = () => {
-        render();   
+        render();
+
+        const form = document.querySelector(".js-form");
+
+        form.addEventListener("submit", onFormSubmit);
     };
 
 
-init();
-
-
+    init();
 
 }
